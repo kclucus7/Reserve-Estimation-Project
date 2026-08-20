08cf561c2c9969d15a36f24059adf34ad1dd2829
kclucus7 <kclucus7@gmail.com>
2026-08-20 14:02:43 -0400

    minor changes

--- gen_payment_lag.py
+++ gen_payment_lag.py
@@ -1,31 +1,34 @@
 import numpy as np 
 import pandas as pd
 
-def gen_payment_lags(claim_sizes): 
+def gen_payment_lags(claim_sizes, claims_number): 
     payment_lags = []
 
-    # Log-transforming the sizes ensures smooth transitions between the means + 
-    # sds as they grow large, so we can scale a linear model
+    # Log-transforming the sizes ensures smooth transitions between the means 
+    # sds as they grow extremely large and we can scale a linear model
     log_sizes = np.log(claim_sizes)
+
      
-    # Calibrating Mu: 
+    # Increasing mu and sigma to scale with log claim sizes to represent the 
+    # increased payment lag and volatility in the lag depending on the size 
+    # (and thereby complexity) of a claim
+
+    # Mu and Sigma values with parameters based on testing file
     mu_intercept = 0.62
     mu_slope = 0.33
     mus = mu_intercept + mu_slope*log_sizes
-    
-    # Calibrating Sigma:
     sigma_intercept = -0.02
     sigma_slope = 0.042
-    sigmas = sigma_intercept + sigma_slope*log_sizes
-    
+    sigmas = sigma_intercept + sigma_slope * log_sizes
+
     # Floor sigma slightly to prevent mathematically impossible zero/negative variance on tiny claims
     sigmas = np.maximum(0.10, sigmas)
 
-    # 3. Vectorized Simulation Run
-    # numpy handles arrays for both parameters seamlessly, ensuring every claim 
-    # gets its own custom probability curve based on its precise cost.
-    payment_lags = np.random.lognormal(mean=mus, sigma=sigmas)
+    # Simulates payment lags according to a log-normal distribution with 
+    # variable parameters depending on the claim size as calculated above. 
+    # Log-normal dsitribution was chosen to cluster payment lags toward the 
+    # short-term while maintaining a portion of longer payment lags in response 
+    # to larger, more complicated claims
+    payment_lags = np.random.lognormal(mus, sigmas)
     
     return payment_lags
-
-
